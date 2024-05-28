8f013db7d7261fb4a3eada39031039c8cff5b77b
Jacinth <jacinthcedricbarral@gmail.com>
2024-05-28 08:02:58 +0800

    Started on a window app with lance, abais, and laroco

--- src/helloworld.java
+++
@@ -1,6 +0,0 @@
-class helloworld {
-    public static void main(String[] args) {
-        System.out.print("Hello World!\n");
-        System.out.printf("Self Serving System Kiosk - Final Project using Java + GUI");
-    }
-}
