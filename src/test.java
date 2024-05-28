58ee0121164febbc46cdc110a064390ffdaa0cad
Jacinth <jacinthcedricbarral@gmail.com>
2024-05-28 22:17:13 +0800

    testGui in java swing branch

---
+++ src/test.java
@@ -0,0 +1,33 @@
+//import javax.swing.JFrame;
+//import javax.swing.JPanel;
+//import javax.swing.border.LineBorder;
+//import java.awt.*;
+//
+//class test{
+//
+//
+//    public void createWindow(){
+//
+//    }
+//
+//    public static void main(String[]args){
+//        JFrame window1 = new JFrame();
+//
+//        window1.setSize(625,800);
+//        window1.setTitle("Jolikod");
+//        window1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        window1.setLayout(null);
+//        window1.setVisible(true);
+//        window1.setResizable(false);
+//
+//        JPanel root = new JPanel();
+//
+//        root.setVisible(true);
+////        root.setPreferredSize(new Dimension(500,500));
+//        root.setBorder(new LineBorder(Color.BLACK, 10));
+//        root.setSize(500,500);
+//
+//        window1.add(root);
+//
+//    }
+//}
